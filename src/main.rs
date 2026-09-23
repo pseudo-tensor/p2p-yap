a1c9fa6def5d50f45c047fb115227674a42e1afc
pseudo-tensor <pkarn125@gmail.com>
2026-09-23 19:38:21 +0530

    OOP is in fact evil

--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,32 @@
-use std::net::Ipv4Addr;
-use tokio::net::{TcpListener, TcpStream};
-use tokio_util::{codec::{Framed, LinesCodec}};
-use futures::{StreamExt, SinkExt};
-use std::error::Error;
-use std::collections::HashMap;
+mod peers;
 
-const PORT: u16 = 6767;
-const LISTENER_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
-
-pub struct Peers
-{
-    peer_map: HashMap<String, Ipv4Addr>,
-    channel: Option<Ipv4Addr>,
-}
-
-impl Peers
-{
-    
-    pub async fn process_command(&mut self, cli_input: &mut String) -> Result<bool, Box<dyn Error>>
-    {
-        let args = cli_input.split_once(" ");
-        
-        if let Some((cmd, arg)) = args
-        {
-            let arg = arg.trim();
-            
-            match cmd 
-            {
-                "add" => {
-                    // split arg into => user and ipaddr
-                }
-                "send" => { 
-                    match self.channel {
-                        Some(ch) => self.send_msg(ch, String::from(arg)).await?,
-                        None => println!("No Channel selected: Use channel <name_of_user> to set a channel"),
-                    }
-                },
-                "channel" => {
-                    self.channel = Some(self.peer_map[arg]);
-                },
-                "close" => {
-                    if arg == "channel" {
-                        self.channel = None;
-                    }
-                    else {
-                        println!("Invalid Command: Use --help");
-                    }
-                }
-                "exit" => return Ok(true),
-                "--help" => {
-                    println!("Commands:");
-                    println!("send: Send message to selected channel. Usage: send <msg>");
-                    println!("channel: Set channel(peer) to send messages. Usage: channel <username>");
-                    // TODO: Continue this later
-                },
-                _ => println!("Invalid Command: Use --help"),
-            }
-        }
-        
-        Ok(false)
-    }
-    
-    pub async fn send_msg(&self, dest: Ipv4Addr, msg: String) -> Result<(), Box<dyn Error>>
-    {
-        let stream = TcpStream::connect((dest, PORT)).await?;
-        let mut framed_conn = Framed::new(stream, LinesCodec::new());
-
-        framed_conn.send(msg).await?;
-        Ok(())
-    }
-    
-    /*
-     * Im assuming this moves any accepted connection 
-     * to a non blocking thread and continues listening
-     */
-    pub async fn listen_for_msg(&self) -> Result<(), Box<dyn Error>>
-    {
-        let stream = TcpListener::bind((LISTENER_ADDR, PORT)).await?;
-        
-        loop {
-            let (socket, addr) = stream.accept().await?;
-            
-            tokio::spawn(async move {
-                let mut framed = Framed::new(socket, LinesCodec::new());
-                
-                while let Some(result) = framed.next().await {
-                    match result {
-                        Ok(line) => {
-                            println!("{addr}: {line}");
-                        }
-                        Err(e) => {
-                            eprintln!("{e}");
-                            break;
-                        }
-                    }
-                }
-                println!("Connection closed.");
-            });
-        }
-    }
-}
+use std::{
+    collections::HashMap,
+    error::Error,
+};
+use crate::peers::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>
 {
-    // let args: Vec<String> = std::env::args().collect();
-    // let mut peer_arg_iter =  args.iter();
-    // let _ =  peer_arg_iter.next().unwrap();
-    // let peer_arg =  peer_arg_iter.next().unwrap();
-    
     let mut peers = Peers {
         peer_map: HashMap::new(),
         channel: None,
     };
+    
+    // Listener only thread
+    tokio::spawn(async move {
+        let _ = listen().await;
+    });
 
     let mut exit_flag = false;
     
+    let mut buf = String::new();
     while !exit_flag {
-        let mut buf = String::new();
-        
         // PONDER: Possibility of injection here?
         let _ = std::io::stdin().read_line(&mut buf)?;
-        exit_flag = peers.process_command(&mut buf).await?;
+        exit_flag = process_command(&mut peers, &mut buf).await?;
     }
-    
-    // match peer_arg.as_str() {
-    //     "PEER1" => {
-    //         println!("PEER1");
-    //         peers.send_msg(PEER2, String::from("hallo PEER2")).await?;
-    //     },
-    //     "PEER2" => {
-    //         println!("PEER2");
-    //         peers.listen_for_msg().await?;
-    //     },
-    //     _ => {}
-    // }
+
     Ok(())
 }
---
+++ src/peers.rs
@@ -0,0 +1,110 @@
+use std::{net::Ipv4Addr, str::FromStr};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_util::{codec::{Framed, LinesCodec}};
+use futures::{StreamExt, SinkExt};
+use std::error::Error;
+use std::collections::HashMap;
+
+// does this serve any purpose?
+const PORT: u16 = 6767;
+const LISTENER_ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
+
+pub struct Peers
+{
+    pub peer_map: HashMap<String, Ipv4Addr>,
+    pub channel: Option<Ipv4Addr>,
+}
+
+pub async fn process_command(peers: &mut Peers, cli_input: &mut String) -> Result<bool, Box<dyn Error>>
+{
+    let args = cli_input.split_once(" ");
+    
+    if let Some((cmd, arg)) = args
+    {
+        let arg = arg.trim();
+        
+        match cmd 
+        {
+            "add" => {
+                if let Some((peer_nick, addr_str)) = arg.split_once(" ")
+                {
+                    let parsed_addr = Ipv4Addr::from_str(addr_str);
+                    match parsed_addr {
+                        Ok(addr) => { peers.peer_map.insert(String::from(peer_nick), addr); },
+                        Err(e) => { eprintln!("{e}"); },
+                    }
+                }
+                else {
+                    println!("Invalid command. Use --help to see usage");
+                }
+            }
+            "send" => { 
+                match peers.channel {
+                    Some(ch) => send(ch, String::from(arg)).await?,
+                    None => println!("No Channel selected: Use channel <name_of_user> to set a channel"),
+                }
+            },
+            "channel" => {
+                peers.channel = Some(peers.peer_map[arg]);
+            },
+            "close" => {
+                if arg == "channel" {
+                    peers.channel = None;
+                }
+                else {
+                    println!("Invalid Command: Use --help");
+                }
+            },
+            "exit" => return Ok(true),
+            "--help" => {
+                println!("Commands:");
+                println!("send: Send message to selected channel. Usage: send <msg>");
+                println!("channel: Set channel to send messages. Usage: channel <username>");
+                println!("add: Add new channel to address book. Usage: add <username> <ipv4addr>");
+                println!("close channel: Close current channel");
+                println!("exit: Close program");
+            },
+            _ => println!("Invalid Command: Use --help"),
+        }
+    }
+    
+    Ok(false)
+}
+
+pub async fn send(dest: Ipv4Addr, msg: String) -> Result<(), Box<dyn Error>>
+{
+    let stream = TcpStream::connect((dest, PORT)).await?;
+    let mut framed_conn = Framed::new(stream, LinesCodec::new());
+
+    framed_conn.send(msg).await?;
+    Ok(())
+}
+
+/*
+* Letting OS magic handle concurrency instead of spawning
+* a new thread per connection request since the connections 
+* last for a very short duration
+*/
+pub async fn listen() -> Result<(), Box<dyn Error>>
+{
+    let stream = TcpListener::bind((LISTENER_ADDR, PORT)).await?;
+    
+    loop {
+        let (socket, addr) = stream.accept().await?;
+        let mut framed = Framed::new(socket, LinesCodec::new());
+        
+        while let Some(result) = framed.next().await {
+            match result {
+                Ok(line) => {
+                    // Print user nick instead of addr here
+                    println!("{addr}: {line}");
+                }
+                Err(e) => {
+                    eprintln!("{e}");
+                    break;
+                }
+            }
+        }
+        println!("Connection closed.");
+    }
+}
